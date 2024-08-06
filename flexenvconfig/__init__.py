81bec1efb55ba6270199378ffb088ce6edca9648
famazak <zakfama58@gmail.com>
2024-08-06 15:52:54 -0400

    feat(api): initial setup
    initial project setup; build ABC config object

---
+++ flexenvconfig/__init__.py
@@ -0,0 +1,29 @@
+import os
+from typing import Optional, TypeVar
+
+from abs import ABC, abstractmethod
+
+
+class BaseFlexEnvConfig(ABC):
+    @abstractmethod
+    def __init__(self) -> None:
+        """
+        Abstact constructor, define your configuration values as attributes
+        """
+        pass
+
+    @classmethod
+    def get_env(cls, key: str, default: str) -> str:
+        """
+        Helper method to fetch values from the environment when building config
+        objects
+        """
+        return os.getenv(key, default)
+
+    @abstractmethod
+    def validate(self) -> bool:  # type: ignore[empty-body]
+        """
+        Implement logic that validates that a config object contains the require
+        attributes
+        """
+        pass
